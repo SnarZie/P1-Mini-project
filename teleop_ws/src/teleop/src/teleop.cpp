e8a9a935e0f0b26266fcd04c98936827759d74c6
SnarZie <SnarZie@users.noreply.github.com>
2017-01-08 13:11:40 +0100

    Update teleop.cpp

--- teleop_ws/src/teleop/src/teleop.cpp
+++ teleop_ws/src/teleop/src/teleop.cpp
@@ -1,44 +1,15 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "std_msgs/Char.h"
-#include <iostream>
+#include <ros/ros.h>
+#include <std_msgs/Char.h>
 #include <stdlib.h>
 #include <sstream>
 
-//This program puplishes what the user writes into the chatter node
+//This program puplishes what the user writes onto the chatter topic
 int main(int argc, char *argv[])
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
   ros::init(argc, argv, "talker");
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
+ 
   ros::NodeHandle n;
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
+  
   ros::Publisher chatter_pub = n.advertise<std_msgs::Char>("chatter", 1000);
 
   ros::Rate loop_rate(10);
@@ -46,22 +17,13 @@ int main(int argc, char *argv[])
   while (ros::ok())
   {
     std_msgs::Char msg; //Message object, that can contain data
-    std_msgs::Char msg2;
-
+    
     std::cout << "There are six points\n to go to A: A\n to go to B: B\n to go to C: C\n to go to D: D\n to go to E: E\n to go to F: F\n\n      User input:    ";
-    std::cin >> msg.data, msg2.data; //Add user input to the data of msg
+    std::cin >> msg.data; //Add user input to the data of msg
 
-    ROS_INFO(" [%c] , [%c] ", msg.data, msg2.data);
+    ROS_INFO("%c", msg.data);
 
-    //system("clear");
-    /**
-     * The publish() function is how you send messages. The parameter
-     * is the message object. The type of this object must agree with the type
-     * given as a template parameter to the advertise<>() call, as was done
-     * in the constructor above.
-     */
     chatter_pub.publish(msg);
-    chatter_pub.publish(msg2);
 
     ros::spinOnce();
 
