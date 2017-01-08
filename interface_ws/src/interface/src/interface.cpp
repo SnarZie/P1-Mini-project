121aee0cd21b4c6595e1b616798fdf57006eaa66
SnarZie <SnarZie@users.noreply.github.com>
2017-01-08 13:12:19 +0100

    Update interface.cpp

--- interface_ws/src/interface/src/interface.cpp
+++ interface_ws/src/interface/src/interface.cpp
@@ -1,12 +1,11 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "std_msgs/Char.h"
-#include <iostream>
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <std_msgs/Char.h>
 #include <stdlib.h>
 #include <sstream>
 
 ros::Subscriber chatter_sub;
-ros::Subscriber chatter2_sub;
+ros::Subscriber interface_sub;
 
 void _listen_cb(const std_msgs::Char::ConstPtr& msg)
 {
@@ -40,18 +39,14 @@ void _clear_cb(const std_msgs::String::ConstPtr& msg)
 		system("clear");
 }
 
-
-
 int main(int argc, char *argv[])
 {
-    ros::init(argc, argv, "phantom");
+    ros::init(argc, argv, "interface");
 	
 	ros::NodeHandle n;
         
-		chatter_sub = n.subscribe( "chatter", 10, &_listen_cb);
-		chatter2_sub = n.subscribe( "chatter2", 10, &_clear_cb);
-
-		
+	chatter_sub = n.subscribe( "chatter", 10, &_listen_cb);
+	interface_sub = n.subscribe( "interface", 10, &_clear_cb);
 
 	ros::spin();
 	return 0;
